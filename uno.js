7b4a1c82e6513e02b8ebb40333458136fe18f43b
Hugo Castellanos <hugo.cassil@gmail.com>
2020-08-10 20:04:16 -0300

    Update uno.js
    primeractualización.

--- uno.js
+++ uno.js
@@ -9,5 +9,62 @@ pedir datos por prompt y mostrar por document.write o console.log
 */
 function mostrar()
 {
-	alert("uno");
+	let nombre;
+	let peso;
+	let sexo;
+	let edad;
+	let nomHombrePesado;
+
+	let cantMujeres=0;
+	let promedioTotalEdad=0;
+	let hombreMasPesa=0;
+
+	let acumEdad=0;
+
+	let respuesta="s";
+
+	do 
+	{
+		nombre=prompt("Ingrese nombre: ");
+		
+		do {
+			peso=parseFloat(prompt("Ingrese peso: "));
+
+		} while(isNaN(peso));
+
+		do {
+			sexo=prompt("Ingrese sexo()m/f");
+		} while (!(sexo=="m"||sexo=="f"));
+
+		do {
+			edad=parseInt(prompt("Ingrese edad: "));
+			acumEdad=acumEdad+edad;
+
+		} while (isNaN(edad));
+
+
+			if(sexo=="f")
+			{
+				cantMujeres++;
+			}
+
+
+			if(sexo=="m"&&peso>hombreMasPesa)
+			{
+				hombreMasPesa=peso;
+				nomHombrePesado=nombre;
+			}
+		
+
+
+		respuesta=prompt("Desea ingresar otro dato(s/n)?");
+	} while (respuesta=="s");
+
+	promedioTotalEdad=acumEdad/5;
+
+	console.log("La cantidad de mujeres es: "+cantMujeres);
+	console.log("La edad promedio total es: "+promedioTotalEdad);
+	console.log("El hombre más pesado es: "+nomHombrePesado);
+
+
 }
